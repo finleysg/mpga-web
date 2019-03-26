aa13d6f34d87a2f225ff4ec98c69a91f71547b29
Stuart Finley <stuart.finley@genmills.com>
2019-03-25 20:26:02 -0500

    hard card download icon

--- src/app/tournaments/hard-card/hard-card.component.ts
+++ src/app/tournaments/hard-card/hard-card.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MpgaDataService } from 'src/app/services/mpga-data.service';
 import { Policy } from 'src/app/models/policies';
 import { forkJoin } from 'rxjs';
+import { MpgaDocument } from 'src/app/models/documents';
+import { AppConfigService } from 'src/app/app.config.service';
 
 @Component({
   selector: 'app-hard-card',
@@ -12,19 +14,24 @@ export class HardCardComponent implements OnInit {
 
   rules: Policy[];
   playerInfo: Policy[];
+  hardCard: MpgaDocument;
 
   constructor(
-    private mpgaData: MpgaDataService
+    private mpgaData: MpgaDataService,
+    private appConfig: AppConfigService
   ) { }
 
   ngOnInit() {
-    forkJoin([
-      this.mpgaData.policies('LR'),
-      this.mpgaData.policies('TP')
-    ]).subscribe(results => {
-      this.rules = results[0];
-      this.playerInfo = results[1];
+    this.appConfig.config.subscribe(config => {
+      forkJoin([
+        this.mpgaData.policies('LR'),
+        this.mpgaData.policies('TP'),
+        this.mpgaData.documents({year: config.eventCalendarYear, docType: 'Other'})
+      ]).subscribe(results => {
+        this.rules = results[0];
+        this.playerInfo = results[1];
+        this.hardCard = results[2].filter(d => d.tags.indexOf('Hard Card') >= 0).shift();
+      });
     });
   }
-
 }
